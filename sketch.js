4567b62f7684b3804a056a3fbfe24c161b9013ae
The Coding World <72372253+The-Coding-World@users.noreply.github.com>
2021-07-03 13:09:53 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,14 @@
+var drops = [];
+function setup() {
+  createCanvas(500, 400);
+  for (var i = 0; i < 500; i++) {
+    drops[i] = new Drop();
+  }
+}
+function draw() {
+  background(230, 230, 250);
+  for (var i = 0; i < drops.length; i++) {
+    drops[i].fall();
+    drops[i].show();
+ }
+}
